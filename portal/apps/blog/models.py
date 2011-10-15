71b4f046715aacbe4040008fe755d57b4a2245d7
Brunno Gomes <brunnolgp@gmail.com>
2011-10-15 14:52:41 -0300

    criando modelo post

--- portal/apps/blog/models.py
+++ portal/apps/blog/models.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateField()
+    author = models.CharField()
+
+    def __unicode__(self):
+      return self.title

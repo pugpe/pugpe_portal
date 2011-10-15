92dd3a86b2f6646d9247154a766285f879fa015b
Brunno Gomes <brunnolgp@gmail.com>
2011-10-15 16:20:13 -0300

    trabalhando no model de posts, ja bem adiantado

--- portal/apps/blog/models.py
+++ portal/apps/blog/models.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib.auth.models import User
+from tagging.fields import TagField
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    date = models.DateField()
-    author = models.CharField()
+    title = models.CharField(verbose_name=u'Título', max_length=200)
+    slug = models.SlugField(verbose_name=u'Slug', max_length=200)
+    content = models.TextField(verbose_name=u'Conteúdo')
+    datetime = models.DateTimeField(verbose_name=u'Data', auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name=u'Autor')
+    tags = TagField(verbose_name=u'Tags')
 
     def __unicode__(self):
-      return self.title
+        return self.title
+
+    class Meta:
+        verbose_name = u'Post'
+        ordering = ['-datetime']
--- portal/settings.py
+++ portal/settings.py
@@ -120,6 +120,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+
+    'tagging',
+
+    'portal.apps.blog',
 )
 
 # A sample logging configuration. The only tangible logging

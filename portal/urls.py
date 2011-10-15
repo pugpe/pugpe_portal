092f72819fa6c834deb033b673a48ba64b79d7ef
Mailson Lira <mailson@gmail.com>
2011-10-15 16:26:57 -0300

    Corrigido o problema com o /static/
    A pasta /static/ foi retirada de STATIC_ROOT, para não chocar com a
    STATICFOLDER.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- portal/urls.py
+++ portal/urls.py
@@ -16,9 +16,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-        (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATIC_ROOT}), 
-    )

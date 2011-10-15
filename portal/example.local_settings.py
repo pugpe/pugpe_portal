bcfcc0964da4187c9482bcbee58cfec3f93612e4
Fernando G N Rocha <fernandogrd@gmail.com>
2011-10-15 14:01:36 -0300

    Corpo do projeto

---
+++ portal/example.local_settings.py
@@ -0,0 +1,12 @@
+# Django settings for portal project.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'banco.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}

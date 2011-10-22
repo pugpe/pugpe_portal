9c660d6d611a8b80e82be27c424b607ca4b57ac6
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2011-10-22 08:31:59 -0300

    Cria homepage do portal
    Signed-off-by: Daker Fernandes Pinheiro <dakerfp@gmail.com>

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from portal.apps.blog.models import Post
+
 def home(request):
-    return render_to_response('index.html',context_instance = RequestContext(request))
+    latest_posts = Post.objects.filter()[:6]
+
+    ctx = {'latest_posts': latest_posts }
+    return render_to_response('index.html', ctx,
+                              RequestContext(request))

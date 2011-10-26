efb4c7f096192a7113a863aea1460f255856b3b7
Mailson Lira <mailson@gmail.com>
2011-10-26 19:07:37 -0300

    Adicionado o path /layout
    Como o index.html foi renomeado para guideline.html, este path foi
    adicionado para continuarmos trabalhando no layout.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- portal/urls.py
+++ portal/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'portal.views.home', name='home'),
+    url(r'^layout/$', 'portal.views.guideline', name='layout'),
     # url(r'^portal/', include('portal.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- portal/views.py
+++ portal/views.py
@@ -3,3 +3,6 @@ from django.template import RequestContext
 
 def home(request):
     return render_to_response('index.html',context_instance = RequestContext(request))
+
+def guideline(request):
+    return render_to_response('guideline.html',context_instance = RequestContext(request))

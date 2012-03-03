8b1748377fe12c06191505eee6dffc57cd7c3a9a
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-03-03 15:16:40 -0300

    Adicionado ListView para event_generator
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- portal/apps/event_generator/urls.py
+++ portal/apps/event_generator/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
-from event_generator.views import EventDetailView
+from event_generator.views import EventDetailView, EventListView
 
 urlpatterns = patterns('apps.gallery.views',
-    #url(r'^$', 'list_events', name='albuns'),
+    url(r'^$', EventListView.as_view(), name='events'),
     url(r'^(?P<slug>[-\w]+)/$', EventDetailView.as_view(), name='event'),
 )
--- portal/apps/event_generator/views.py
+++ portal/apps/event_generator/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from event_generator.models import Event
+
+
+class EventListView(ListView):
+    model = Event
+    context_object_name = u'events'
 
 
 class EventDetailView(DetailView):
